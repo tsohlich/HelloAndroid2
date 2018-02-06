0432d1c1382a258afbe9aec19d23469ba6dc74dc
tsohlich <35311271+tsohlich@users.noreply.github.com>
2018-02-06 18:41:32 +0100

    styles updates + strings updated

--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.helloandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void mail(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/html");
+        intent.putExtra(Intent.EXTRA_EMAIL, R.string.email);
+        intent.putExtra(Intent.EXTRA_SUBJECT, R.string.subject);
+        startActivity(Intent.createChooser(intent, "Send"));
     }
 }
